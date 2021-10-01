15aae8560b37c55eaea426fc7cdaaabf03aaa428
rennleon <2829@holbertonschool.com>
2021-10-01 14:00:02 +0000

    Change camel case to snake case

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -16,7 +16,7 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def perimeter_of_my_square(self):
         """Perimeter of the Sqaure"""
         return (self.width * 2) + (self.height * 2)
 
@@ -26,8 +26,7 @@ class square():
 
 
 if __name__ == "__main__":
-
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
