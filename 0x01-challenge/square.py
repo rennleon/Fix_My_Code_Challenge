6ac5534333b7be296d558ad555a5d17dd622499a
rennleon <2829@holbertonschool.com>
2021-10-01 14:22:04 +0000

    Add getters and setters for each attribute

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -5,10 +5,39 @@
 class square():
     """Square class definition"""
 
-    def __init__(self, width=0, height=0):
+    def __init__(self, width, height):
         """Constructor for squeare instances"""
         self.width = width
         self.height = height
+
+    def __validate(self, value):
+        """Data validation for edges"""
+        if type(value) is not int:
+            raise TypeError('Value must be an integer')
+        if value <= 0:
+            raise ValueError('Value must be greater than zero')
+
+    @property
+    def width(self):
+        """Width proerty method"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """"Width property setter"""
+        self.__validate(value)
+        self.__width = value
+
+    @property
+    def height(self):
+        """Width proerty method"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """"Height property setter"""
+        self.__validate(value)
+        self.__height = value
 
     def area_of_my_square(self):
         """ Area of the square """
